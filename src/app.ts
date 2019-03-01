d8484dc5a9e8197b5ce8f13f02a30d5707b2be88
ddz <dongdezhang01@daojia-inc.com>
2019-03-01 15:25:20 +0800

    增加移动端事件

--- src/app.ts
+++ src/app.ts
@@ -8,59 +8,122 @@ import {
 } from './components/square/index'
 import './styles/index.scss'
 import { isPhone, bingEvent } from './utils/index'
-let cas = <HTMLCanvasElement>document.getElementById('gameCanvas'), pw = 0, ph = 0
+interface EventAction {
+  target: HTMLElement
+  id: number
+  action: Function
+  interval: number
+}
+interface OperationArray {
+  [index: string]: EventAction
+}
+let cas = <HTMLCanvasElement>document.getElementById('gameCanvas'),
+  pw = 0,
+  ph = 0
 let acw = <HTMLElement>document.querySelector('.actions')
+let loadingToast = <HTMLElement>document.getElementById('loadingToast')
+let loadButton = <HTMLElement>document.getElementById('loadButton')
+let progressDiv = <HTMLElement>document.getElementById('progressDiv')
+let source = <HTMLElement>document.getElementById('source')
+let fps = <HTMLElement>document.getElementById('fps')
+let gameTime = <HTMLElement>document.getElementById('gameTime')
+let restart = <HTMLElement>document.getElementById('restart')
+let gameOver = <HTMLElement>document.getElementById('gameOver')
+let up = <HTMLElement>document.querySelector('.act-btn.up')
+let down = <HTMLElement>document.querySelector('.act-btn.down')
+let left = <HTMLElement>document.querySelector('.act-btn.left')
+let right = <HTMLElement>document.querySelector('.act-btn.right')
+let res = [
+  '青铜水平',
+  '白银水平',
+  '黄金水平',
+  '铂金水平',
+  '钻石水平',
+  '星耀水平',
+  '最强王者',
+  '荣耀王者'
+]
+let t = Date.now()
 
-if(isPhone()) {
+if (isPhone()) {
   cas.width = 275
   cas.height = 375
   pw = 150
   ph = 25
   acw.style.display = 'block'
-}else {
+} else {
   cas.width = 550
   cas.height = 750
   pw = 300
   ph = 25
   acw.style.display = 'none'
 }
+let progressbar = new ProgressBar(
+  pw,
+  ph,
+  'rgba(0,0,0,0.5)',
+  '#ef4510',
+  '#00964b'
+)
+let game = new EluosiGame('俄罗斯方块', 'gameCanvas')
+let sp = new SquareSprite(
+  'square sprite',
+  SquareSpritePainter,
+  [SquareSpriteBehavior],
+  game,
+  1,
+  { x: 0, y: 5 }
+)
+const frep = 50
+const ed: OperationArray = {
+  up: {
+    target: up,
+    id: 0,
+    interval: frep * 4,
+    action: () => {
+      game.keyPressed({ keyCode: 38 })
+    }
+  },
+  down: {
+    target: down,
+    id: 0,
+    interval: frep,
+    action: () => {
+      game.keyPressed({ keyCode: 40 })
+    }
+  },
+  left: {
+    target: left,
+    id: 0,
+    interval: frep * 4,
+    action: () => {
+      game.keyPressed({ keyCode: 37 })
+    }
+  },
+  right: {
+    target: right,
+    id: 0,
+    interval: frep * 4,
+    action: () => {
+      game.keyPressed({ keyCode: 39 })
+    }
+  }
+}
+gameOver.style.display = 'none'
+restart.style.display = 'none'
 
-let game = new EluosiGame('俄罗斯方块', 'gameCanvas'),
-  loadingToast = <HTMLElement>document.getElementById('loadingToast'),
-  loadButton = <HTMLElement>document.getElementById('loadButton'),
-  progressDiv = <HTMLElement>document.getElementById('progressDiv'),
-  progressbar = new ProgressBar(
-    pw,
-    ph,
-    'rgba(0,0,0,0.5)',
-    '#ef4510',
-    '#00964b'
-  ),
-  source = <HTMLElement>document.getElementById('source'),
-  fps = <HTMLElement>document.getElementById('fps'),
-  gameTime = <HTMLElement>document.getElementById('gameTime'),
-  restart = <HTMLElement>document.getElementById('restart'),
-  gameOver = <HTMLElement>document.getElementById('gameOver')
-  gameOver.style.display = 'none'
-  restart.style.display = 'none'
-  let up = <HTMLElement>document.querySelector('.act-btn.up')
-  let down = <HTMLElement>document.querySelector('.act-btn.down')
-  let left = <HTMLElement>document.querySelector('.act-btn.left')
-  let right = <HTMLElement>document.querySelector('.act-btn.right')
 
-  bingEvent(up, 'click', () => {
-    game.keyPressed({keyCode: 38})
+
+for (let key in ed) {
+  bingEvent(ed[key].target, 'touchstart', () => {
+    ed[key].action()
+    ed[key].id = setInterval(ed[key].action, ed[key].interval)
   })
- 
-  bingEvent(down, 'click', () => {
-    game.keyPressed({keyCode: 40})
+  bingEvent(ed[key].target, 'touchend', () => {
+    clearInterval(ed[key].id)
   })
-  bingEvent(left, 'click', () => {
-    game.keyPressed({keyCode: 37})
-  })
-  bingEvent(right, 'click', () => {
-    game.keyPressed({keyCode: 39})
-  })
+}
+
 restart.addEventListener('click', function() {
   setTimeout(() => {
     game.restart()
@@ -71,13 +134,11 @@ restart.addEventListener('click', function() {
 loadButton.addEventListener(
   'click',
   function(e) {
-    let interval: number = 0,
+    let interval = 0,
       loadingPercentComplete = 0
 
     e.preventDefault()
-
     loadButton.style.display = 'none'
-
     progressDiv.appendChild(progressbar.domElement)
 
     interval = setInterval(function() {
@@ -95,24 +156,15 @@ loadButton.addEventListener(
   },
   false
 )
-let sp = new SquareSprite(
-  'square sprite',
-  SquareSpritePainter,
-  [SquareSpriteBehavior],
-  game,
-  1,
-  { x: 0, y: 5 }
-)
-let res = ['青铜水平','白银水平','黄金水平','铂金水平','钻石水平','星耀水平','最强王者','荣耀王者']
-let t = Date.now()
+
 setInterval(() => {
   source.innerText = game.source + ''
-  if(Date.now() - t > 500) {
+  if (Date.now() - t > 500) {
     fps.innerText = Math.ceil(game.fps) + ''
     t = Date.now()
   }
-  if(game.status === 'end') {
-    if(game.source > 400)  gameOver.innerText = 'Game Over, ' + res[7]
+  if (game.status === 'end') {
+    if (game.source > 400) gameOver.innerText = 'Game Over, ' + res[7]
     else gameOver.innerText = 'Game Over, ' + res[Math.ceil(game.source / 50)]
     gameOver.style.display = 'block'
     restart.style.display = 'block'
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,35 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.base.js')
+const os = require('os')
+
+function get_ip() {
+  let needHost = ''
+  try {
+    let network = os.networkInterfaces()
+    for (let dev in network) {
+      let iface = network[dev]
+      for (let i = 0; i < iface.length; i++) {
+        let alias = iface[i]
+        if (
+          alias.family === 'IPv4' &&
+          alias.address !== '127.0.0.1' &&
+          !alias.internal
+        ) {
+          needHost = alias.address
+        }
+      }
+    }
+  } catch (e) {
+    needHost = 'localhost'
+  }
+  return needHost
+}
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    host: get_ip(),
     contentBase: './docs'
   }
 })
